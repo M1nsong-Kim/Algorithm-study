e72d4998e05fdc4ddab1ec88546603035b960774
minsong kim <lgms.3222@gmail.com>
2022-08-28 22:05:20 +0900

    first commit

---
+++ CountOfFactors.java
@@ -0,0 +1,9 @@
+package codingtestAndAlgorithm;
+
+public class CountOfFactors {
+	int solution(int left, int right) {
+		int answer = 0;
+		int [] array = new int[];
+		return answer;
+	}
+}
---
+++ SumOfFactorsExam.java
@@ -0,0 +1,10 @@
+package codingtestAndAlgorithm;
+
+public class SumOfFactorsExam {
+
+	public static void main(String[] args) {
+		SumOfFactors sof = new SumOfFactors();
+		System.out.println(sof.solution(5));
+	}
+
+}
