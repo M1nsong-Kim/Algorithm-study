37635e452c3a324c4adf544058a523abe309fffc
M1nsong-Kim <lgms.3222@gmail.com>
2022-09-21 23:07:14 +0900

    Algorithm-Day25

---
+++ Day25/Day25_B_2750.java
@@ -0,0 +1,29 @@
+package codingtestAndAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Day25_B_2750 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int arr[] = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		for(int i = 0; i < n; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
