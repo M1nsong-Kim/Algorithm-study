9142eff9f599118c3a503b651fcafc249f59a32c
M1nsong-Kim <lgms.3222@gmail.com>
2022-11-12 14:30:35 +0900

    Algorithm-Day77

---
+++ Day77/Day77BOJ2566.java
@@ -0,0 +1,30 @@
+package codingtestAndAlgorithm;
+
+import java.util.Scanner;
+
+public class Day77BOJ2566 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[][] arr = new int[9][9];
+		
+		// 배열에 넣을 필요 없이 처음부터 끝까지 크기 비교
+		int max = 0;
+		int row = 0;
+		int col = 0;
+		for(int i = 0; i < 9; i++) {	// 행
+			for(int j = 0; j < 9; j++) {	//열
+				int n = sc.nextInt();
+				if(max < n) {
+					max = n;
+					
+					row = i+1;
+					col = j+1;
+				}
+			}
+		}
+		System.out.print(max + "\n" + row + " " + col);
+	}
+
+}
