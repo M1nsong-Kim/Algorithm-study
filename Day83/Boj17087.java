34fc4f5c23d4f451ce1756872e31e1e183e509b8
M1nsong-Kim <lgms.3222@gmail.com>
2022-11-18 22:35:46 +0900

    Algorithm-Day83-timeImprove

--- Day83/Boj17087.java
+++ Day83/Boj17087.java
@@ -1,26 +1,29 @@
 package codingtestAndAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Boj17087 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int n = sc.nextInt();
-		int s = sc.nextInt();
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
 		
 		ArrayList<Integer> list = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) {
 			//주어진 숫자에서 수빈이의 위치를 빼고 리스트에 추가한다
-			list.add(Math.abs(sc.nextInt()-s));		
+			list.add(Math.abs(Integer.parseInt(st.nextToken())-s));		
 			
 			//디버깅
-			System.out.println(list.get(i));
+			//System.out.println(list.get(i));
 		}
-		
-
 		
 		int result = list.get(0);
 		for(int i = 1; i < list.size(); i++) {
