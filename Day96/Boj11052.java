2f1080c0db979eb7838004e8751d28e814fff92f
M1nsong-Kim <lgms.3222@gmail.com>
2022-11-30 23:01:40 +0900

    Algorithm-Day96-success

--- Day96/Boj11052.java
+++ Day96/Boj11052.java
@@ -1,9 +1,6 @@
-package codingtestAndAlgorithm;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Boj11052 {
@@ -15,20 +12,22 @@ public class Boj11052 {
 		int n = Integer.parseInt(br.readLine());
 		
 		// 인덱스에 n이 들어가야 하므로 배열의 크기는 n+1
-		int[] max = new int[n+1];	//최댓값 구할 배열
+		int[] dp = new int[n+1];	//최댓값 구할 배열
 		int[] pack = new int[n+1];
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 1; i < n+1; i++) {
 			pack[i] = Integer.parseInt(st.nextToken());
 		}
-		//System.out.println(Arrays.toString(pack));
+		// System.out.println(Arrays.toString(pack)); 디버깅
 		
-		for(int i = 0; i < n+1; i++) {
-			max[i] = pack[i] + pack[n-i];
+		for(int i = 1; i < n+1; i++) {
+			for(int j = 1; j < i+1; j++) {
+				dp[i] = Math.max(dp[i], pack[j]+dp[i-j]);
+			}
 		}
-		Arrays.sort(max);
-		System.out.println(max[n]);
+
+		System.out.println(dp[n]);
 	}
 
 }
