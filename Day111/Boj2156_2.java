890e699fb016bde9d0b5646d8da7519b7d4cea59
M1nsong-Kim <lgms.3222@gmail.com>
2022-12-15 23:17:58 +0900

    Algorithm Day111 Boj2156 포도주 시식 코드 추가

---
+++ Day111/Boj2156_2.java
@@ -0,0 +1,31 @@
+package codingtestAndAlgorithm;
+
+import java.io.*;
+
+public class Boj2156_2 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n+1];
+		int[] dp = new int[n+1];
+		
+		for(int i = 1; i < n+1; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		// n = 1부터이므로 아래는 무조건 성립
+		dp[1] = arr[1];
+		// 와인잔이 2개 이상일 때
+		if(n > 1) {
+			dp[2] = arr[1]+arr[2];			
+		}
+		for(int i = 3; i < n+1; i++) {
+			dp[i] = Math.max(dp[i-1], Math.max(dp[i-2]+arr[i], dp[i-3]+arr[i-1]+arr[i]));
+		}
+		System.out.println(dp[n]);
+		
+	}
+}
+// 과정 https://gimbalja.tistory.com/267
