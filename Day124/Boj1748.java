7de702e31082de289972d9f36b3bfeb6c5478d81
M1nsong-Kim <lgms.3222@gmail.com>
2022-12-28 23:19:34 +0900

    Algorithm Day124 BOJ1748 수 이어 쓰기 1 improved

--- Day124/Boj1748.java
+++ Day124/Boj1748.java
@@ -10,26 +10,14 @@ public class Boj1748 {
 		int n = Integer.parseInt(br.readLine());
 		
 		int count = 0;
+		int cipher = 1;	// 자릿수 ( 1~10: 1, 11~100: 2, ...
+		int standard = 10;
 		for(int i = 1; i <= n; i++) {
-			if(i < 10) {
-				count++;
-			}else if(i < 100) {
-				count += 2;
-			}else if(i < 1_000) {
-				count += 3;
-			}else if(i < 10_000) {
-				count += 4;
-			}else if(i < 100_000) {
-				count += 5;
-			}else if(i < 1_000_000) {
-				count += 6;
-			}else if(i < 10_000_000) {
-				count += 7;
-			}else if(i < 100_000_000) {
-				count += 8;
-			} else {	//if(i == 100_000_000) {
-				count += 9;
+			if(i % standard == 0) {
+				cipher++;	// 기준으로 나누어 떨어진다 -> 자릿수가 바뀌었다 -> 자릿수 +1
+				standard *= 10; // 10, 100, 1000 ... 으로 기준을 늘린다(자릿수 바뀌는 수가 기준)
 			}
+			count += cipher;
 		}
 		
 		System.out.println(count);
