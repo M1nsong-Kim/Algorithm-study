6d718048468ce37aeef0635ab665249b09682c4c
M1nsong-Kim <lgms.3222@gmail.com>
2022-12-31 17:16:10 +0900

    Algorithm Day127 BOJ15651 N과 M(3) improved

--- Day127/Boj15651.java
+++ Day127/Boj15651.java
@@ -7,7 +7,6 @@ public class Boj15651 {
 	
 	static int n, m;
 	static int[] arr;
-	static boolean[] visited;
 	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException{
@@ -19,7 +18,6 @@ public class Boj15651 {
 		m = Integer.parseInt(st.nextToken());
 		
 		arr = new int[m];
-		visited = new boolean[n];
 		
 		DFS(0);
 		bw.write(sb.toString());
@@ -38,12 +36,8 @@ public class Boj15651 {
 		
 		
 		for(int i = 0; i < n; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				arr[depth] = i+1;
-				visited[i] = false;	// 재귀를 호출하기 전에 false로 바꾸면 중복 숫자도 들어간다
-				DFS(depth+1);
-			}
+			arr[depth] = i+1;
+			DFS(depth+1);
 		}
 	}
 
