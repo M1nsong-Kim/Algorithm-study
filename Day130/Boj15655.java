1e9976eebc50846afb4a52f3f6ca1e7f8770592b
M1nsong-Kim <lgms.3222@gmail.com>
2023-01-03 16:11:38 +0900

    Algorithm Day130 BOJ15655 N과 M(6)

---
+++ Day130/Boj15655.java
@@ -0,0 +1,60 @@
+package codingtestAndAlgorithm.Day130;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj15655 {
+	
+	static int n, m;
+	static int[] arr;
+	static int[] nums;	// 주어지는 수를 입력받기 위한 배열 생성
+	static BufferedWriter bw;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		nums = new int[n];	
+		visited = new boolean[10_001];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(nums);	// 사전순대로 출력하기 위해 오름차순 정렬
+		
+		DFS(0);
+		bw.flush();
+		bw.close();
+	}
+	
+	static void DFS(int depth) throws IOException{
+		if(depth == m) {
+			for(int val : arr) {
+				bw.write(val+" ");
+			}
+			bw.newLine();
+			return;
+		}
+		
+		for(int i = 0; i < n; i++) {
+			if(!visited[nums[i]]) {	// 이전 i 자리에 nums[i]를 넣어 입력 받은 숫자만 들어가도록
+				visited[nums[i]] = true;
+				arr[depth] = nums[i];
+				DFS(depth+1);
+				for(int j = i+1; j < n; j++) {
+					if(visited[nums[j]]) {		
+						visited[nums[j]] = false;
+					}
+				}
+			}
+		}
+	}
+
+}
+// 과정 https://gimbalja.tistory.com/297
