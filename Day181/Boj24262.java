b26cabcc67243be0063ba52df5f917355e506cf0
M1nsong-Kim <lgms.3222@gmail.com>
2023-02-24 21:39:40 +0900

    Day181 BOJ24262 알고리즘 수업 - 알고리즘의 수행 시간1

---
+++ Day181/Boj24262.java
@@ -0,0 +1,14 @@
+package codingtestAndAlgorithm.Day181;
+
+import java.io.*;
+
+public class Boj24262 {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(1+"\n");
+		bw.write(0+"");
+		bw.flush();
+		bw.close();
+	}
+}
