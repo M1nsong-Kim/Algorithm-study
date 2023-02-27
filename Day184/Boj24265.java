ef3cd4e4ce6136dc2602be66959b75d5ef4c822a
M1nsong-Kim <lgms.3222@gmail.com>
2023-02-27 15:23:20 +0900

    Day184 BOJ24265 알고리즘 수업 - 알고리즘의 수행 시간4 공식 적용

--- Day184/Boj24265.java
+++ Day184/Boj24265.java
@@ -10,13 +10,8 @@ public class Boj24265 {
 		
 		long n = Integer.parseInt(br.readLine()); 
 		// 최댓값 500,000의 제곱은 int의 범위 이상 -> long으로 커버
-		long sum = 0;
 		
-		for(int i = 1; i < n; i++) {
-			sum += i;
-		}
-		
-		bw.write(sum+"\n");
+		bw.write(n*(n-1)/2+"\n");
 		bw.write(2+"");
 		bw.flush();
 		bw.close();
