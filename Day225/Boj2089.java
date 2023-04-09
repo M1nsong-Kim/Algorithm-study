5ddb32f0fc172722a46ed5f8614b2f9cb7bb37cb
M1nsong-Kim <lgms.3222@gmail.com>
2023-04-09 17:59:16 +0900

    Day225 BOJ2089 -2진수(복습)

---
+++ Day225/Boj2089.java
@@ -0,0 +1,31 @@
+package codingtestAndAlgorithm.Day225;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj2089 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+		
+		if(n == 0) {
+			System.out.println(0);
+		}else {
+			while(n != 1) {
+				sb.append(Math.abs(n % -2));
+				n = (int)Math.ceil((double)n/-2);	// ★
+			}
+			
+			sb.append(n);
+			System.out.println(sb.reverse());
+		}
+		
+		br.close();
+	}
+
+}
+// 복습 과정 https://gimbalja.tistory.com/425
